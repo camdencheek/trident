4394281a7952fa1f4524010969a18ccfad89ead3
Camden Cheek <camden@ccheek.com>
2022-10-17 17:12:44 -0600

    roundtrip deltas

--- src/serialize.rs
+++ src/serialize.rs
@@ -123,6 +123,70 @@ impl<R: Read> U32Decompressor<R> {
     }
 }
 
+pub struct U32DeltaDecompressor<R: Read> {
+    r: R,
+    remaining: usize,
+    chunk: [u32; BitPacker4x::BLOCK_LEN],
+    chunk_range: Range<usize>,
+    buf: [u8; BitPacker4x::BLOCK_LEN * 4],
+}
+
+impl<R: Read> Iterator for U32DeltaDecompressor<R> {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.chunk_range.next() {
+            Some(n) => Some(self.chunk[n]),
+            None => {
+                self.populate_next_chunk();
+                Some(self.chunk[self.chunk_range.next()?])
+            }
+        }
+    }
+}
+
+impl<R: Read> U32DeltaDecompressor<R> {
+    fn new(r: R, count: usize) -> Self {
+        Self {
+            r,
+            remaining: count,
+            chunk: [0u32; BitPacker4x::BLOCK_LEN],
+            chunk_range: 0..0,
+            buf: [0u8; BitPacker4x::BLOCK_LEN * 4],
+        }
+    }
+
+    fn populate_next_chunk(&mut self) {
+        if self.remaining >= BitPacker4x::BLOCK_LEN {
+            let bp = BitPacker4x::new();
+            let num_bits = {
+                let mut buf = [0; 1];
+                self.r.read_exact(&mut buf).unwrap();
+                buf[0]
+            };
+            let num_bytes = num_bits as usize * BitPacker4x::BLOCK_LEN / 8;
+            self.r.read_exact(&mut self.buf[..num_bytes]).unwrap();
+            let n = bp.decompress_sorted(
+                self.chunk[BitPacker4x::BLOCK_LEN - 1],
+                &self.buf[..num_bytes],
+                &mut self.chunk,
+                num_bits,
+            );
+            self.chunk_range = 0..BitPacker4x::BLOCK_LEN;
+            assert!(n == num_bytes);
+            self.remaining -= BitPacker4x::BLOCK_LEN;
+        } else {
+            let mut last = self.chunk[BitPacker4x::BLOCK_LEN - 1];
+            for i in 0..self.remaining {
+                self.chunk[i] = self.r.read_varint::<u32>().unwrap() + last;
+                last = self.chunk[i];
+            }
+            self.chunk_range = 0..self.remaining;
+            self.remaining = 0;
+        }
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -133,6 +197,21 @@ mod test {
             let mut buf = Vec::new();
             U32Compressor(input.as_slice()).write_to(&mut buf).unwrap();
             let decompressor = U32Decompressor::new(Cursor::new(buf), input.len());
+            let output: Vec<u32> = decompressor.collect();
+            input == output
+        }
+    }
+
+    quickcheck! {
+        fn compress_roundtrip_delta(input: Vec<u16>) -> bool {
+            let mut sum: u32 = 0;
+            let input: Vec<u32> = input.iter().map(|x| {
+                sum += *x as u32;
+                sum
+            }).collect();
+            let mut buf = Vec::new();
+            U32DeltaCompressor(input.as_slice()).write_to(&mut buf).unwrap();
+            let decompressor = U32DeltaDecompressor::new(Cursor::new(buf), input.len());
             let output: Vec<u32> = decompressor.collect();
             input == output
         }
