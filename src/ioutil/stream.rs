1df5f04968e69146437ee7cb4c912fa8c6a5ffde
Camden Cheek <camden@ccheek.com>
2022-11-02 16:41:46 -0600

    add StreamRead trait

--- src/ioutil/stream.rs
+++ src/ioutil/stream.rs
@@ -1,6 +1,7 @@
-use std::io::{self, Write};
+use anyhow::Result;
+use std::io::{self, Read, Write};
 
-use byteorder::{BigEndian, ByteOrder, WriteBytesExt};
+use byteorder::{BigEndian, ReadBytesExt, WriteBytesExt};
 
 pub trait StreamWrite {
     fn write_to<W: Write>(&self, w: &mut W) -> io::Result<usize>;
@@ -45,5 +46,44 @@ impl<const N: usize> StreamWrite for [u8; N] {
     fn write_to<W: Write>(&self, w: &mut W) -> io::Result<usize> {
         w.write(self)?;
         Ok(N)
+    }
+}
+
+pub trait StreamRead
+where
+    Self: Sized,
+{
+    fn read_from<R: Read>(r: &mut R) -> Result<Self>;
+}
+
+impl StreamRead for u8 {
+    fn read_from<R: Read>(r: &mut R) -> Result<Self> {
+        Ok(r.read_u8()?)
+    }
+}
+
+impl StreamRead for u16 {
+    fn read_from<R: Read>(r: &mut R) -> Result<Self> {
+        Ok(r.read_u16::<BigEndian>()?)
+    }
+}
+
+impl StreamRead for u32 {
+    fn read_from<R: Read>(r: &mut R) -> Result<Self> {
+        Ok(r.read_u32::<BigEndian>()?)
+    }
+}
+
+impl StreamRead for u64 {
+    fn read_from<R: Read>(r: &mut R) -> Result<Self> {
+        Ok(r.read_u64::<BigEndian>()?)
+    }
+}
+
+impl<const N: usize> StreamRead for [u8; N] {
+    fn read_from<R: Read>(r: &mut R) -> Result<Self> {
+        let mut buf = [0u8; N];
+        r.read(&mut buf)?;
+        Ok(buf)
     }
 }
